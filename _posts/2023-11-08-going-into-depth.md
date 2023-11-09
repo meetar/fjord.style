70dbc036ebc5a23d5d2531ac7b77e07f6f8ba63f
Peter Richardson <pxrich@gmail.com>
2023-11-08 18:08:45 -0800

    reorg

---
+++ _posts/2023-11-08-going-into-depth.md
@@ -0,0 +1,114 @@
+---
+layout: page
+title: Going Into Depth
+subtitle: A 3D Trick
+categories: 3d
+published: true
+excerpt: This post examines a variation on parallax mapping that I used for a recent project called <a href="/the-prototype-trap">The Gem Collector</a>, with detailed examinations of the rationale, the technique in the abstract, and its implementation in detail.
+image: 'speckles.gif'
+imgalt: 'A closeup of a 3D rendered crystal'
+---
+
+<link rel="stylesheet" href="assets/crystal-shader/demo.css">
+
+<!-- 
+Let's try something new! Mix and match your own adventure:
+
+<button>Child</button> <button>Artist</button> <button>Coder</button> <button>Specialist</button>
+
+---
+-->
+
+<div class="aside">This post examines a variation on parallax mapping that I used for <a href="/the-prototype-trap">a recent project</a>, with detailed examinations of the rationale, the technique in the abstract, and its implementation in detail.</div>
+
+<br>
+
+Most 3D objects rendered by computers are defined as hollow shells, a collection of zero-thickness surfaces, describing empty volumes. But the world is full of solids, each with its own inner life of nooks and crannies, cracks and dings, secret layers, bubbles, inclusions, and imperfections.
+
+So I made a magic brush that paints inside of things! I made it, specifically, to add sparkles to crystals. Here's a stripped-down example:
+
+
+<div class="iframewrapper">
+<iframe class="glcanvas" src="https://meetar.github.io/FS-reverse-parallax-plain/"></iframe>
+<div class="aside">[<a href="https://github.com/meetar/FS-reverse-parallax-plain/">code</a>]</div>
+</div>
+
+This crystal uses a shader that manipulates textures on an object's surface, based on the view angle and a heightmap. This makes certain parts of the texture appear to be more distant than the faces they're applied to, suggesting internal structures which would otherwise require a much more detailed mesh to render.
+
+(For an introduction to shaders, I recommend <a href="http://thebookofshaders.com">The Book of Shaders</a>.)
+
+Let's break down the effect.
+
+# Perspective Shift
+
+This trick is entirely based on the <em>parallax effect</em>, which makes distant objects appear to move more slowly. Conceptually, it's a very simple idea – you can even fake it in CSS! Drag the box below:
+
+<div id="scrollContainer" class="container">
+  <div id="scrollDiv" class="box"></div>
+</div>
+
+This box appears to be a window to a separate, more-distant starry layer, which is really just a repeating <code>background-image</code> on the moving div. The background position is being changed in <em>linear proportion</em> to the div's position, but more slowly. This relationship creates the parallax effect. Here's the line of JavaScript that sets the relevant css properties for the example:
+
+<code>div.style.backgroundPositionX = - div.getBoundingClientRect().left/2 + 'px';</code>
+
+The `-` sign shows that the background is being moved in the <em>opposite direction</em> of the div, as otherwise the background would inherit the div's motion. The result is that the opposing motion adds a bit of "drag". The `/2` sets the rate of the background's motion to half the rate of the div itself, which suggests distance. (If it were `/1`, the backwards motion would equal the div's motion, and the background would appear to be motionless.)
+
+This effect also works during rotation, which can also be simulated in CSS. Drag this box to see it in action:
+
+<div id="rotateContainer" class="container">
+  <div id="rotateDiv" class="box"></div>
+</div>
+
+In the example above, the background position is moved relative to <em>the tangent</em> of the div's rotation. This approximates the relationship between rotating layers of different distance from the viewer.
+
+<code>div.style.backgroundPositionX = - Math.tan(divRotationInRadians)/2 + 'px';</code>
+
+If you can imagine the rotating div above as a face in a 3D mesh, this is the basic principle of the crystal shader. Here's one more CSS prototype, using multiple copies of the last example as faces of a cube, and applying 3D transforms:
+
+<div id="cubeContainer" class="container">
+  <div id="cube" class="cubeDiv">
+    <div class="face front"></div>
+    <div class="face left"></div>
+    <div class="face right"></div>
+    <div class="face top"></div>
+    <div class="face bottom"></div>
+  </div>
+</div>
+
+The net effect is something like refraction, as though the object were a solid cube of glass in front of a distant star field.
+
+# Turning up the Volume
+
+However, to add the illusion of <em>volume</em> to the shape, we have to provide depth cues at various distances. To do this, we can simulate a parallax effect between individual stars.
+
+There are many ways this could be done. I chose a method which uses a single image as both heightmap and texture map. In other words, information from the same bitmap is used for both displacement and color.
+
+Here's a simple example using canvas elements to split the image up into layers of constant brightness, and CSS transformations to simulate the parallax between layers, with one important difference from the crystal's method: brighter pixels appear to be pulled <em>towards</em> the viewer.
+
+<div id="cheeseContainer" class="container"></div>
+
+This is similar to the standard behavior of <em>parallax shaders</em>, a class of shaders used to add the illusion of depth to a surface. Generally, they assume a continuous, opaque surface, and are used to add small amounts of subtle <em>protruding</em> detail to an otherwise flat face – bricks and cobblestones are a very common use case. With these shaders, two bitmaps are generally used: one for color and one as a heightmap.
+
+We're looking for something slightly different, but most of the same principles apply. In fact, you could describe the crystal shader as a parallax shader turned inside out:
+
+<div id="cheeseContainerReverse" class="container"></div>
+
+You'll notice that in the previous examples, "closer" layers occlude the "further" layers. Occlusion could be useful in certain cases, but when dealing with sparkly crystals, you normally want to maximize the sparkle levels.
+
+To achieve this (and get around the occlusion problem) the crystal shader uses a technique which equates to a "lighten" blending mode in a layer-based image editor like Photoshop. This is done in CSS with `mix-blend-mode: lighten`, and the result is that where pixels overlap, the lightest pixel is shown. (To see anything we'll need to ensure that the background is darker than the foreground.)
+
+<div id="cheeseContainerLighten" class="container"></div>
+
+This is the essence of the technique! It's relatively simple to do this on a flat plane, but to do this on an arbitrary 3D object, we'll need something more powerful than CSS, capable of slightly more intense math.
+
+In the next post, we'll walk through the building blocks of the implementation in a shader in the abstract, and then we'll dig into the shader code itself.
+
+Until then, enjoy a shiny purple gem. Thanks for reading!
+
+<div class="iframewrapper">
+<iframe class="glcanvas" src="https://meetar.github.io/FS-reverse-parallax"></iframe>
+</div>
+
+
+<script src="assets/crystal-shader/demo.js"></script>
+
---
+++ assets/going-into-depth/demo.js
@@ -0,0 +1,277 @@
+let rotationConstraint = 4;
+
+let scrollDiv = document.getElementById("scrollDiv");
+let rotateDiv = document.getElementById("rotateDiv");
+
+//
+// scrolling parallax example
+//
+
+function parallaxScroll(x) {
+  let bbox = scrollDiv.getBoundingClientRect();
+  scrollDiv.style.left  = `calc(${x}px - ${bbox.width/2}px)`;
+  scrollDiv.style.backgroundPositionX = - x/2 + 'px';
+}
+
+
+//
+// rotation parallax example
+//
+
+function parallaxRotate(x) {
+  let bbox = rotateDiv.getBoundingClientRect();
+  let rotateY = (x - bbox.x - (bbox.width / 2)) / rotationConstraint;
+  rotateY = Math.max(rotateY, -85);
+  rotateY = Math.min(rotateY, 85);
+
+  // 10000px approximates a very long lens
+  rotateDiv.style.transform  = "perspective(10000px) "
+    + "   rotateX("+ -10 +"deg) "
+    + "   rotateY("+ rotateY +"deg) "
+
+  // prevent the background offset from being calculated if the frame is at its limit
+  if (Math.abs(rotateY) == 85) return;
+
+  // calculate tangent of the angle in radians
+  const angleInRadians = (rotateY * Math.PI) / 180;
+  const tanAngle = Math.tan(angleInRadians);
+  // offset background position by the tangent times an arbitrary multiplier
+  const bgoffset = -tanAngle * 50;
+
+  // apply new background image position to xoffset only
+  rotateDiv.style.backgroundPosition = bgoffset + 'px 0px';
+}
+
+
+//
+// 3D cube example
+//
+
+
+const cube = document.getElementById('cube');
+
+// CSS cube rotation and backgroundPosition
+function updateCubeRotation(clientX, clientY) {
+  const { left, top, width, height } = cube.getBoundingClientRect();
+
+  const center = {
+    x: left + width / 2 + 50, // 50 to center the cube in its parent
+    y: top + height / 2,
+  };
+
+  const deltaX = clientX - center.x;
+  const deltaY = clientY - center.y;
+
+  // rotation sensitivity
+  const rotateY = deltaX * 0.1;
+  const rotateX = deltaY * -0.1;
+
+  const transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateX(-50px) translateY(-50px)`; // translate to center manually
+  cube.style.transform = transform;
+
+  const faces = cube.querySelectorAll('.face');
+  faces.forEach(face => {
+    // use a linear relationship between rotation and background position for simplicity –
+    // this prevents switching cases for each face to handle proper tangent orientation
+    let bgPosX = -rotateY; // flip this here because the background origin (0,0) is at the top
+    let bgPosY = rotateX;
+    face.style.backgroundPosition = `${bgPosX}px ${bgPosY}px`;
+  });
+}
+
+
+// 
+// canvas parallax example
+// 
+
+const image = new Image();
+image.src = 'assets/crystal-shader/speckles2.png';
+const cheeseContainer = document.getElementById('cheeseContainer');
+const cheeseContainerReverse = document.getElementById('cheeseContainerReverse');
+
+image.onload = function () {
+  // create a canvas to manipulate the image
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d', { willReadFrequently: true });
+
+  const imageWidth = image.width;
+  const imageHeight = image.height;
+  const numRanges = 7; // number of brightness ranges
+  const step = 256 / numRanges; // calculate step for splitting into ranges
+
+  for (let range = 0; range < numRanges; range++) {
+    canvas.width = imageWidth;
+    canvas.height = imageHeight;
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const pixels = imageData.data;
+
+    // define brightness range for this canvas
+    const minBrightness = range * step;
+    const maxBrightness = (range + 1) * step;
+
+    // iterate through pixels and set alpha based on brightness range – 
+    // array is a Uint8ClampedArray with a stride of 4: r, g, b, a
+    for (let i = 0; i < pixels.length; i += 4) {
+      const brightness = pixels[i]; // only need to check one channel to calculate brightness
+
+      // check if the brightness falls within the specified range
+      if (brightness >= minBrightness && brightness < maxBrightness) {
+        pixels[i + 3] = 255; // fully opaque
+      } else {
+        pixels[i + 3] = 0; // fully transparent
+      }
+    }
+
+    // write modified image data back to the canvas
+    ctx.putImageData(imageData, 0, 0);
+
+    // create a new canvas element for the page, containing only pixels from the current brightness range
+    const newCanvas = document.createElement('canvas');
+    newCanvas.willReadFrequently = true;
+
+    newCanvas.className = 'cheeseSlice';
+    newCanvas.width = canvas.width;
+    newCanvas.height = canvas.height;
+    newCanvas.getContext('2d').drawImage(canvas, 0, 0);
+
+    // append the canvas to the cheeseContainer
+    cheeseContainer.appendChild(newCanvas);
+
+    
+    // prepend the canvas to the reversed cheeseContainers
+    ['cheeseContainerReverse', 'cheeseContainerLighten'].forEach(selector => {
+      // clone the canvas element
+      const clonedCanvas = cloneCanvas(newCanvas);
+      const div = document.getElementById(selector);
+      div.insertBefore(clonedCanvas, div.firstChild);
+    });
+  }
+
+};
+
+function cloneCanvas(canvas) {
+  const clonedCanvas = canvas.cloneNode();
+
+  // Create a context for the cloned canvas
+  const clonedContext = clonedCanvas.getContext('2d');
+
+  // Set the dimensions of the cloned canvas
+  clonedCanvas.width = canvas.width;
+  clonedCanvas.height = canvas.height;
+
+  // Copy the content from the original canvas to the cloned canvas
+  clonedContext.drawImage(canvas, 0, 0);
+  return clonedCanvas
+}
+
+// range of motion modulation, from least motion to most motion (furthest to nearest for parallax effect)
+const startFactor = .1;
+const endFactor = .2;
+
+function updateCheese(clientX, clientY, selector) {
+  const containerDiv = document.getElementById(selector);
+  const canvasList = document.querySelectorAll(`#${selector} > .cheeseSlice`);
+  const numCanvases = canvasList.length;
+  const rect = containerDiv.getBoundingClientRect();
+  var mouseX = clientX - rect.left ;
+  var mouseY = clientY - rect.top;
+
+  // if the mouse is too far away, set the input position to the center of the container div so all offsets are 0,0
+  if (Math.abs(mouseY) > 300) {
+    mouseY = rect.height / 2;
+    mouseX = rect.width / 2;
+  }
+
+  const centerX = containerDiv.clientWidth / 2;
+  const centerY = containerDiv.clientHeight / 2;
+
+  for (let i = 0; i < numCanvases; i++) {
+    const canvas = canvasList[i];
+    const factor = startFactor + (endFactor - startFactor) * (i / (numCanvases - 1));
+
+    const offsetX = (mouseX - centerX) * factor;
+    const offsetY = (mouseY - centerY) * factor;
+
+    canvas.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  }
+};
+
+
+//
+// event handlers
+//
+
+function handleScroll(e) {
+  // handle mouse and touch events
+  const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+  e.preventDefault();
+  window.requestAnimationFrame(() => {
+    parallaxScroll(clientX);
+  });
+}
+
+function handleRotate(e) {
+  // handle mouse and touch events
+  const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+  e.preventDefault();
+  window.requestAnimationFrame(() => {
+    parallaxRotate(clientX);
+  });
+}
+
+// track whether touchstart event started within the allowed divs
+let touchStartedInsideDiv = false;
+
+// set flag if the touch starts inside the allowed divs
+document.querySelectorAll('#cubeContainer, #cheeseContainer').forEach(div => {
+    div.addEventListener('touchstart', function() {
+      touchStartedInsideDiv = true;
+  });
+});
+
+// reset the flag when the touch ends
+document.addEventListener('touchend', function() {
+  touchStartedInsideDiv = false;
+});
+
+// Reset the flag if the touch is cancelled
+document.addEventListener('touchcancel', function() {
+  touchStartedInsideDiv = false;
+});
+
+function handleDocumentMove(e) {
+  // handle mouse and touch events
+  const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+  const clientY = e.touches ? e.touches[0].clientY : e.clientY;
+
+  // handle mobile behavior
+  if (e.touches) {
+    if (touchStartedInsideDiv) {
+    // don't scroll on mobile
+    e.preventDefault()
+    } else {
+      // don't rotate cube if touch started outside of the container
+      return;
+    }
+  }
+
+  window.requestAnimationFrame(() => {
+    updateCubeRotation(clientX, clientY);
+    ['cheeseContainer', 'cheeseContainerReverse', 'cheeseContainerLighten'].forEach(selector => {
+      updateCheese(clientX, clientY, selector);
+    })
+  });
+
+}
+
+// mouse event listeners
+document.getElementById("rotateContainer").addEventListener('mousemove', handleRotate);
+document.getElementById("scrollContainer").addEventListener('mousemove', handleScroll);
+document.addEventListener('mousemove', handleDocumentMove);
+
+// touch event listeners
+document.getElementById("rotateContainer").addEventListener('touchmove', handleRotate, { passive: false });
+document.getElementById("scrollContainer").addEventListener('touchmove', handleScroll, { passive: false });
+document.addEventListener('touchmove', handleDocumentMove, { passive: false });
